c69c0df6709a63b7be131f4a606431090d3e9ae2
N-Red <63408242+N-Red@users.noreply.github.com>
2021-08-27 11:49:59 +0200

    change last request GET (with DTO) + add Jacoco dependency

--- src/test/java/com/safetynet/safetynetalerts/serviceTest/PersonServiceTest.java
+++ src/test/java/com/safetynet/safetynetalerts/serviceTest/PersonServiceTest.java
@@ -1,7 +1,5 @@
 package com.safetynet.safetynetalerts.serviceTest;
 
-import com.safetynet.safetynetalerts.service.PersonService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,6 +13,5 @@ public class PersonServiceTest {
 
     @Test
     void getListTest() {
-
     }
 }
